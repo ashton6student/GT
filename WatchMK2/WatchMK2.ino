1b6496772241a4499c811e1ad130aabe20b407dc
Ashtonrb <78123522+Ashtonrb@users.noreply.github.com>
2024-05-18 17:34:28 -0500

    Added other base screens, haven't implimented alarm or stopwatch

--- WatchMK2/WatchMK2.ino
+++ WatchMK2/WatchMK2.ino
@@ -10,37 +10,59 @@
 #define SCL 22
 
 //Peripheral Pins
-#define btn1PIN 34
-#define btn2PIN 35
+#define btn1PIN 33
+#define btn2PIN 32
+#define btn3PIN 35
 
 //Variables
-int curSec = 0;
-int curMin = 0;
-int curHour = 0;
+/////////////////////
+//General variables
+  //Peripherals
+  int curBtn1 = 0;
+  int oldBtn1 = 0;
+  int btn1Pressed;
 
-String hourOut, minOut, secOut;
-String hourBlink, minBlink, secBlink;
+  int curBtn2 = 0;
+  int oldBtn2 = 0;
+  int btn2Pressed;
 
-//Usage variables
-int state = 0;
-int newState = LOW;
-int btn2Pressed;
-int iterate = HIGH;
-int counter = 0;
+  int curBtn3 = 0;
+  int oldBtn3 = 0;
+  int btn3Pressed;
 
-int curBtn1 = 0;
-int oldBtn1 = 0;
+  //Timing variables
+  unsigned long prevTime1 = 0;
+  unsigned long prevTime2 = 0;
+  unsigned long prevTime3 = 0;
+  
+  const long inv1 = 1000; // Interval for task1 (1 second)
+  const long inv2 = 10; // Interval for task2 (0.01 second)
+  const long inv3 = 500; // Interval for task2 (0.5 second)
+  
+  //Control
+  int state = 0; //0 - Clock, 1 - Alarm, 2 - Stopwatch, 3 - Set Clock
+  
+//State 0 - Clock variables
+  int curSec = 0;
+  int curMin = 0;
+  int curHour = 0;
 
-int curBtn2 = 0;
-int oldBtn2 = 0;
+  String hourOut, minOut, secOut, tag;
+  
+//State 1 - Alarm variables
+  int alrMin = 0;
+  int alrHour = 0;
+  
+//State 2 - Stopwatch variables
+  int stpMil = 0;
+  int stpSec = 0;
+  int stpMin = 0;
 
-//Timing variables
-unsigned long prevTime1 = 0;
-unsigned long prevTime2 = 0;
-unsigned long prevTime3 = 0;
-const long inv1 = 1000; // Interval for task1 (1 second)
-const long inv2 = 10; // Interval for task2 (0.01 second)
-const long inv3 = 500; // Interval for task2 (0.5 second)
+//State 3 - Set Clock variables
+  int setState; //0 - Don't set, 1 - set hour, 2 - set minute, 3 - reset second
+  int iterate = HIGH;
+  String hourBlink, minBlink, secBlink;
+/////////////////////
 
 //OLED Display
 Adafruit_SSD1306 display(128, 64, &Wire, -1);
@@ -56,110 +78,239 @@ void setup() {
   //Display Setup
   display.begin(SSD1306_SWITCHCAPVCC, OLED_ADDR); //Turns on display
   display.setTextColor(1);
-  display.setTextSize(2);
-  display.setCursor(20, 20);
 
   //Peripheral Setup
   pinMode(btn1PIN, INPUT);
-  pinMode(btn2PIN, INPUT);  
+  pinMode(btn2PIN, INPUT);
+  pinMode(btn3PIN, INPUT);  
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   unsigned long currentTime = millis();
 
+  //Occurs every second. 
   if(currentTime - prevTime1 >= inv1) {
+    //Save current time
     prevTime1 = currentTime;
+    //Occurs For All States
     iterateTime(curHour, curMin, curSec);
+    //State Select
+    switch(state) {
+      case 0: //Clock state
+        break;
+      case 1: //Alarm state
+        break;
+      case 2: //Stopwatch state
+        break;
+      case 3: //Set Clock state
+        break;
+      default:
+        break;
+    }
   }
 
+  //Occurs every 0.01 seconds - USED FOR: Setting setState, setting display numbers.
   if(currentTime - prevTime2 >= inv2) {
     prevTime2 = currentTime;
+    
+    //State Select
+    checkPeripherals();
     stateSelect();
-    setNum();
+    switch(state) {
+      case 0: //Clock state
+        break;
+      case 1: //Alarm state
+        break;
+      case 2: //Stopwatch state
+        break;
+      case 3: //Set Clock state
+        setStateSelect();
+        setNum();
+        break;
+      default:
+        break;
+    }
   }
 
-  if(newState == HIGH){
+  //Occurs every quarter second - USED FOR: Blinking in Set Clock state.
+  if(btn1Pressed == HIGH){
     prevTime3 = currentTime;
   } else {
       if(currentTime - prevTime3 >= inv3) {
         prevTime3 = currentTime;
-        blinkNum();
-      }  
+      //State Select
+      switch(state) {
+        case 0: //Clock state
+          break;
+        case 1: //Alarm state
+          break;
+        case 2: //Stopwatch state
+          break;
+        case 3: //Set Clock state
+          blinkNum();
+          break;
+        default:
+          break;
+      }
+    }  
   }
-  
-  //Generate Outputs
-  String output = hourOut + ":" + minOut + ":" + secOut;
-  
   // Display the result 
-  display.clearDisplay(); // Clear display buffer
-  display.setCursor(10, 30); // Set cursor position
-  display.println(output); // Print output string
-  display.display(); // Send buffer to display
-  Serial.println(iterate); // Print output string
+  displayWatch();
 }
 
-void stateSelect() {
-  curBtn1 = digitalRead(btn1PIN);
-  if(curBtn1 == HIGH && oldBtn1 == LOW){
-    newState = HIGH;
-    if(state == 3) {
-      state = 0;
-    } else {
-      state++;
-    }
-  } else {
-    newState = LOW;
-  }
-  oldBtn1 = curBtn1;
+//General Functions
+///////////////////////////////
+void displayWatch() {
   switch(state) {
-    case 1:
-      hourOut = hourBlink;
-      minOut  = appendZero(curMin);
-      secOut  = appendZero(curSec);      
-      break;
-    case 2:
-      hourOut = appendZero(curHour);
-      minOut  = minBlink;
-      secOut  = appendZero(curSec);      
-      break;
-    case 3:
-      hourOut = appendZero(curHour);
-      minOut  = appendZero(curMin);
-      secOut  = secBlink;
-      break;
-    default:
+    case 0: //Clock state
+      tag = "Clock";
       hourOut = appendZero(curHour);
       minOut  = appendZero(curMin);
       secOut  = appendZero(curSec);
       break;
-  }  
+    case 1: //Alarm state
+      tag = "Alarm";
+      hourOut = "11";
+      minOut  = "11";
+      secOut  = "11";
+      break;
+    case 2: //Stopwatch state
+      tag = "Stopwatch";
+      hourOut = "22";
+      minOut  = "22";
+      secOut  = "22";
+      break;
+    case 3: //Set Clock state
+      tag = "Set Clock";
+        switch(setState) {
+          case 0:
+            hourOut = hourBlink;
+            minOut  = appendZero(curMin);
+            secOut  = appendZero(curSec);      
+            break;
+          case 1:
+            hourOut = appendZero(curHour);
+            minOut  = minBlink;
+            secOut  = appendZero(curSec);      
+            break;
+          case 2:
+            hourOut = appendZero(curHour);
+            minOut  = appendZero(curMin);
+            secOut  = secBlink;
+            break;
+          default:
+            hourOut = appendZero(curHour);
+            minOut  = appendZero(curMin);
+            secOut  = appendZero(curSec);
+            break;
+        }  
+      break;
+    default:
+      break;
+  }
+  String output = hourOut + ":" + minOut + ":" + secOut;
+  display.clearDisplay(); // Clear display buffer
+
+  //Draw Tag
+  display.setCursor(0, 0); // Set cursor position
+  display.setTextSize(1);
+  display.println(tag); // Print output string
+
+  //Draw Date
+  display.setCursor(70, 0); // Set cursor position
+  display.setTextSize(1);
+  display.print("5/18/2024"); // Print output string  
+  
+  //Draw First Set of Numbers
+  display.setCursor(0, 40); // Set cursor position
+  display.setTextSize(3);
+  display.print(hourOut + ":" + minOut); // Print output string
+
+  //Draw Second Set of Numbers
+//  display.setCursor(0, 35); // Set cursor position
+  display.setTextSize(2);
+  display.println(":" + secOut); // Print output string
+  display.display(); // Send buffer to display
+  Serial.println("btn1: " + String(digitalRead(btn1PIN)) + " btn2: " + String(digitalRead(btn2PIN)) + " btn3: " + String(digitalRead(btn3PIN))); // Print output string
+  Serial.println(state); // Print output string  
+}
+
+void checkPeripherals() {
+  curBtn1 = digitalRead(btn1PIN);
+  curBtn2 = digitalRead(btn2PIN);
+  curBtn3 = digitalRead(btn3PIN);
+  
+  if(curBtn1 == HIGH && oldBtn1 == LOW)
+    btn1Pressed = HIGH;
+  else
+    btn1Pressed = LOW;
+    
+  if(curBtn2 == HIGH && oldBtn2 == LOW)
+    btn2Pressed = HIGH;
+  else
+    btn2Pressed = LOW;
+    
+  if(curBtn3 == HIGH && oldBtn3 == LOW)
+    btn3Pressed = HIGH;
+  else
+    btn3Pressed = LOW;
+    
+  oldBtn1 = curBtn1; 
+  oldBtn2 = curBtn2; 
+  oldBtn3 = curBtn3;  
+}
+///////////////////////////////
+
+//State - 0 Clock Functions
+///////////////////////////////
+void stateSelect() {
+  if(btn3Pressed){
+    if(state == 3)
+      state = 0;
+    else
+      state++;
+  }
+}
+///////////////////////////////
+
+//State - 1 Alarm Functions
+///////////////////////////////
+///////////////////////////////
+
+//State - 2 Stopwatch Functions
+///////////////////////////////
+///////////////////////////////
+
+//State - 3 Set Clock Functions
+///////////////////////////////
+void setStateSelect() {
+  if(btn1Pressed){
+    if(setState == 2)
+      setState = 0;
+    else
+      setState++;
+  }
 }
 
 void setNum() {
-  curBtn2 = digitalRead(btn2PIN);
   if(curBtn2 == LOW){
     iterate = HIGH;
   }
-  if(curBtn2 == HIGH && oldBtn2 == LOW){
-    btn2Pressed = HIGH;
-  } else {
-    btn2Pressed = LOW;
-  }
-  switch(state) {
-    case 1:
+  switch(setState) {
+    case 0:
       if(btn2Pressed && iterate) {
         iterateHour(curHour);
         iterate = LOW; 
       }
       break;
-    case 2:
+    case 1:
       if(btn2Pressed && iterate) {
         iterateMin(curMin);
         iterate = LOW; 
       }   
       break;
-    case 3:
+    case 2:
       if(btn2Pressed && iterate) {
         curSec = 0;
         iterate = LOW; 
@@ -171,8 +322,8 @@ void setNum() {
 }
 
 void blinkNum() {
-  switch(state) {
-    case 1:
+  switch(setState) {
+    case 0:
       if(hourBlink == "  "){
         hourBlink = appendZero(curHour);
       } else {
@@ -181,7 +332,7 @@ void blinkNum() {
       minBlink  = "  ";
       secBlink  = "  ";
       break;
-    case 2:
+    case 1:
       hourBlink = "  ";
       if(minBlink == "  "){
         minBlink = appendZero(curMin);
@@ -190,7 +341,7 @@ void blinkNum() {
       }
       secBlink  = "  "; 
       break;
-    case 3:
+    case 2:
       hourBlink = "  ";
       minBlink  = "  ";    
       if(secBlink == "  "){
@@ -206,7 +357,10 @@ void blinkNum() {
       break;
   }    
 }
+///////////////////////////////
 
+//General Utility Functions
+///////////////////////////////
 String appendZero(const int &num) {
   String result = "";
   if(num < 10){
@@ -261,3 +415,4 @@ void iterateTime(int &hour, int &minute, int &second) {
     hour = 0;
   }   
 }
+///////////////////////////////
